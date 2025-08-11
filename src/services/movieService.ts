ad6a1432fc59687c620d9dea6fc5ac5b280107d1
Petya0610 <160774838+Petya0610@users.noreply.github.com>
2025-08-11 19:58:10 +0200

    fix

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,17 +4,22 @@ import type { Movie } from "../types/movie";
 const API_URL = "https://api.themoviedb.org/3/search/movie";
 const API_TOKEN = import.meta.env.VITE_TMDB_API_KEY;
 
-interface fetchMoviesResponse {
+interface FetchMoviesResponse { 
   results: Movie[];
 }
 
-export const fetchMovie = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  if (!API_TOKEN) {
+    throw new Error("TMDB API token is missing");
+  }
+
   const config = {
     params: { query },
     headers: {
       Authorization: `Bearer ${API_TOKEN}`,
     },
   };
-  const response = await axios.get<fetchMoviesResponse>(API_URL, config)
-  return response.data.results
-}
+
+  const response = await axios.get<FetchMoviesResponse>(API_URL, config);
+  return response.data.results;
+};
